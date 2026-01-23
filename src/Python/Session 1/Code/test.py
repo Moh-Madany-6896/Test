a4628f0fe0411813ebd18056c7061d934473d172
Moh-Madany-6896 <aucid2050@gmail.com>
2026-01-23 22:02:02 +0200

    s

---
+++ src/Python/Session 1/Code/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as mt
+
+path = "C:/Users/aucid/Downloads/train.csv"
+
+dafr = pd.read_csv (path)
+
+dafr.head ()
+
+# def Handle (data):
+#     dafr.drop
